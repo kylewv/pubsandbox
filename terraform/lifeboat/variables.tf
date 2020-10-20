bbeaa5796c20f4199a8a9dcac3e0fc63dc71aeb1
Kyle Veldhuizen <kveldhuizen@carleton.edu>
2020-10-20 14:41:34 -0500

    ligeboat terraform test code

---
+++ terraform/lifeboat/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_access_key" {
+  default = ""
+}
+variable "aws_secret_key" {
+  default = ""
+}
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "domain_name" {
+  default = "status.kylewv.com"
+}
+
+variable "website_bucket_name" {
+  default = "kylewv-static"
+}
